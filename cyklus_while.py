2adaac5fdfcd7f0abff1a17a4e3b5ed3eaf2d4e9
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-17 18:42:58 +0200

    ukazka cyklu while

---
+++ cyklus_while.py
@@ -0,0 +1,13 @@
+# cyklus pokracuje dokud je nejaka podminka TRUE
+x = 0
+while x <= 10:
+    print(f"Ja jsem {x} cyklus while.") # ctrl +c pro preruseni
+    x += 1
+print("Jedeme dal.")
+
+
+
+
+
+
+
