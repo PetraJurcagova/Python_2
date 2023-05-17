232cae9f9957f17775ea7130f6cbc48526daa62b
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-17 19:00:07 +0200

    vylepseni o nahodu

--- hledame_postavy.py
+++ hledame_postavy.py
@@ -1,8 +1,17 @@
-# hadaci hra
-character = "Harry"
+# hadaci hra, vylepseni o nahodu
+import random
+characters= ["Harry", "Ron", "Hermionna", "Brumbal"]
+character = characters[random.randint(0, len(characters)-1)]
 guess = " "
 
 while character != guess: # dokud je podminka TRUE, tak cyklus jede
-    guess = input("Uhodnete postavu z filmu Harry Potter\n")
+    guess = input("Uhodnete postavu z filmu Harry Potter.\n")
 
 print("Uhadli jste, vybordelne!")   
+
+
+
+
+
+
+
