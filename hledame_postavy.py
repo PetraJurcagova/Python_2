63222a1a3a2a84a3407f940b82f312e8d8d107eb
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-17 19:19:49 +0200

    vylepseni o pocet pokusu a kontrolu spravne odpovedi

--- hledame_postavy.py
+++ hledame_postavy.py
@@ -1,13 +1,27 @@
-# hadaci hra, vylepseni o nahodu
+# hadaci hra, vylepseni o nahodu, vylepseni o pocet pokusu
 import random
+
+print("Vitejte v hadaci hre Harry Potter")
+
 characters= ["Harry", "Ron", "Hermionna", "Brumbal"]
 character = characters[random.randint(0, len(characters)-1)]
 guess = " "
 
-while character != guess: # dokud je podminka TRUE, tak cyklus jede
-    guess = input("Uhodnete postavu z filmu Harry Potter.\n")
+guess_count = 2 # nastaveny pocet pokusu
 
-print("Uhadli jste, vybordelne!")   
+while character != guess: # dokud je podminka TRUE, tak cyklus jede
+    if guess_count != 0: # jeste mame moznost hadat
+        guess = input("Uhodnete postavu z filmu Harry Potter.\n")
+        guess_count -= 1
+    else:
+        print(f"Neuhadli jste! Hledane jmeno bylo {character}.")
+        break # preruseni cyklu, ktery je jinak nekonecny
+
+    if guess == character: # kontrola spravne odpovedi
+        print("Uhadli jste, vybordelne!")   
+
+
+
 
 
 
