201d83857af8afa764dd1ea38154c78105a50442
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-28 14:22:18 +0200

    napoveda docstring a jeji vytvoreni

---
+++ vlastni_napoveda.py
@@ -0,0 +1,9 @@
+# docstring = napoveda k funkcim  """ a sem napiseme text"""
+
+def sum(num1, num2):
+    """vrati soucet dvou cisel"""
+    return num1 + num2
+
+
+vypocet = sum(2,30)
+print(vypocet)
