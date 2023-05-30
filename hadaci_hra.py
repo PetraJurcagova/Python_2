ed813c5ec93344c440fa54e2aabc3a5609796842
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-30 18:53:49 +0200

    dokonceni hadaci hry

--- hadaci_hra.py
+++ hadaci_hra.py
@@ -6,7 +6,7 @@ print("Vitejte ve hre Guess Secret Number")
 print("Myslim si cislo od 1 do 100")
 
 # vyreseni obtiznosti
-difficulty = input("Vyberte obtiznost: 'easy' nebo 'hard':\n")
+
 
 #priprava hry
 # potrebujeme secret number
@@ -14,30 +14,48 @@ difficulty = input("Vyberte obtiznost: 'easy' nebo 'hard':\n")
 secret_number = random.randint(1, 101)
 print(f"Hadane cislo je {secret_number}.")
 
+def obtiznost(): # muzeme a nemusime z toho udelat funkci
+    difficulty = input("Vyberte obtiznost: 'easy' nebo 'hard':\n")
+    if difficulty == "easy":
+        return 7
+    elif difficulty == "hard":
+        return 4
 
-
+def guessing_game():
 # pocet pokusu
-pokusy = 0 # vstupni udaj
-if difficulty == "easy":
-    pokusy = 7
-elif difficulty == "hard":
-    pokusy = 4
+    
+    pokusy = obtiznost() # vstupni udaj
+   
 #print(f"Pocet zbyvajicich pokusu je {pokusy}.") # kontrolni vypis
 
+# rekurze
 
+    another_game = ""
+    
+    while pokusy > 0:
+        print(f"Pocet zbyvajicich pokusu je {pokusy}.")
+        guess_user = int(input("Typnete si cislo:\n"))
 
-while pokusy > 0:
-    print(f"Pocet zbyvajicich pokusu je {pokusy}.")
-    guess_user = int(input("Typnete si cislo:\n"))
+        if guess_user < secret_number:
+            print("Cislo je prilis nizke.")
+            pokusy -= 1 # odecitame pokusy
+        elif guess_user > secret_number:
+            print("Cislo je prilis vysoke.")
+            pokusy -= 1
+        else:
+            print("Uhadli jste! Pocitac je porazen.")
+            another_game = input("Napiste 'yes', pokud chcete pokracovat. Napiste 'no', pokud chcete hru ukoncit.")
+            
+        # kontrola poctu pokusu   
+        if pokusy == 0:
+            print("Prohrali jste, pocitac vyhral!")
+            another_game = input("Napiste 'yes', pokud chcete pokracovat. Napiste 'no', pokud chcete hru ukoncit.")
 
-    if guess_user < secret_number:
-        print("Cislo je prilis nizke.")
-        pokusy -= 1 # odecitame pokusy
-    elif guess_user > secret_number:
-        print("Cislo je prilis vysoke.")
-        pokusy -= 1
-    else:
-        print("Uhadli jste! Pocitac je porazen.")
-     # kontrola poctu pokusu   
-    if pokusy == 0:
-        print("Prohrali jste, pocitac vyhral!")
+        if another_game == "yes":   
+            os.system("cls") # vycisteni obrazovky
+            guessing_game() # toto je rekurze, funkce je ve funkci, tj. cela se spusti znovu
+        elif another_game == "no":
+            os.system("cls")
+            break # ukonceni cyklu
+
+guessing_game()
