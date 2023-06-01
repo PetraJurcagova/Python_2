50de1ecc40e5e377ebdaec47181b3b06a97aea07
Petra Jurcagova <jurcagova.p@gmail.com>
2023-06-01 13:41:37 +0200

    funkce a moznosti porovnani

--- pokrocila_hra.py
+++ pokrocila_hra.py
@@ -11,8 +11,11 @@ def account_generator(all_accounts):
 
     
 account_1 = account_generator(data)
-
 account_2 = account_generator(data)
 
+#predelani na funkci, ktera printuje moznosti k porovnani
+def printing_options(acc1, acc2):
+    print(f"Porovnejte A: {acc1['name']}, {acc1['description']}, {acc1['country']} ")
+    print(f"Porovnejte B: {acc2['name']}, {acc2['description']}, {acc2['country']} ")
 
-
+printing_options(account_1, account_2)
