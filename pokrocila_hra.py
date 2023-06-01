bcb7b2ea03183e2e12ae040b64361a7aee2e97b4
Petra Jurcagova <jurcagova.p@gmail.com>
2023-06-01 14:19:17 +0200

    dokonceni hry

--- pokrocila_hra.py
+++ pokrocila_hra.py
@@ -1,6 +1,6 @@
 from data import data
 import random
-
+#import os
 
 # predelani na funkci
 def account_generator(all_accounts):
@@ -9,39 +9,54 @@ def account_generator(all_accounts):
     random_number =  random.randint (0, data_len -1)
     return all_accounts [random_number]
 
-    
-account_1 = account_generator(data)
-account_2 = account_generator(data)
-
-#predelani na funkci, ktera printuje moznosti k porovnani
+    #predelani na funkci, ktera printuje moznosti k porovnani
 def printing_options(acc1, acc2):
     print(f"Porovnejte A: {acc1['name']}, {acc1['description']}, {acc1['country']} ")
     print(f"Porovnejte B: {acc2['name']}, {acc2['description']}, {acc2['country']} ")
 
-printing_options(account_1, account_2)
-
-
-#testovani
-print(f"testovaci vypis ucet 1: {account_1['follower_count']}")
-print(f"testovaci vypis ucet 2: {account_2['follower_count']}")   
-
 # hra
-user_answer = input("Kdo ma vice sledovani moznost 'A' nebo 'B'?\n")
+def game():
+    lets_continue = True
+    right_a = ""
+    score = 0
 
-right_a = ""
-score = 0
+    account_1 = account_generator(data)
+    account_2 = account_generator(data)
+    
+# pokus o vylouceni opakovani stejneho uctu v ramci jedne nabidky
+    if account_1 == account_2:
+        account_2 = account_generator(data)
 
-if account_1["follower_count"] > account_2["follower_count"]:
-    right_a = "A"
-else:
-    right_a = "B"
+    while lets_continue:
+        printing_options(account_1, account_2)
 
-if right_a == user_answer:
-    score += 1
-    print(f"Uhadli jste, vase skore je {score}.")
-else:
-    print(f"Spatna odpoved, vase konecne skore je {score} bodu.")
+          #testovani
+        print(f"testovaci vypis ucet 1: {account_1['follower_count']}")
+        print(f"testovaci vypis ucet 2: {account_2['follower_count']}")   
 
+        user_answer = input("Kdo ma vice sledovani moznost 'A' nebo 'B'?\n")
+
+       
+        if account_1["follower_count"] > account_2["follower_count"]:
+            right_a = "A"
+            
+        else:
+            right_a = "B"
+            
+        account_1 = account_2
+
+        if right_a == user_answer:
+            score += 1
+            print(f"Uhadli jste, vase skore je {score}.")
+            account_2 = account_generator(data)
+
+        else:
+            print(f"Spatna odpoved, vase konecne skore je {score} bodu.")
+            lets_continue = False # zastaveni cyklu
+            #os.system("cls") # vycisteni obrazovky
+
+# zavolani funkce       
+game()
 
 
 
