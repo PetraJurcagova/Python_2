4039211d504312a87fd89e4427a4ecf541d99cf7
Petra Jurcagova <jurcagova.p@gmail.com>
2023-06-01 13:53:51 +0200

    prvni cast hry

--- pokrocila_hra.py
+++ pokrocila_hra.py
@@ -19,3 +19,29 @@ def printing_options(acc1, acc2):
     print(f"Porovnejte B: {acc2['name']}, {acc2['description']}, {acc2['country']} ")
 
 printing_options(account_1, account_2)
+
+
+#testovani
+print(f"testovaci vypis ucet 1: {account_1['follower_count']}")
+print(f"testovaci vypis ucet 2: {account_2['follower_count']}")   
+
+# hra
+user_answer = input("Kdo ma vice sledovani moznost 'A' nebo 'B'?\n")
+
+right_a = ""
+score = 0
+
+if account_1["follower_count"] > account_2["follower_count"]:
+    right_a = "A"
+else:
+    right_a = "B"
+
+if right_a == user_answer:
+    score += 1
+    print(f"Uhadli jste, vase skore je {score}.")
+else:
+    print(f"Spatna odpoved, vase konecne skore je {score} bodu.")
+
+
+
+
